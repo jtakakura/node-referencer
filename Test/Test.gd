2030e995180098df6dcec94f172bb067feacfd17
folt-a <faultun@gmail.com>
2023-05-20 11:21:33 +0900

    add shortcut and fixed unique name path

--- Test/Test.gd
+++ Test/Test.gd
@@ -1,18 +1,12 @@
 extends Node2D
 
-### Automatic References Start ###
-@onready var _animated_sprite: AnimatedSprite2D = $Node2D/AnimatedSprite2D
-@onready var _kinematic_body_2d: CharacterBody2D = $Node2D/CharacterBody2D
-@onready var _node_2d: Node2D = $Node2D
-@onready var _node_2d_1: Node2D = $ToolScript/Node2D
-@onready var _node_2d_2: Node2D = $ToolCustomeClassScript/Node2D
-@onready var _tool_custome_class_script: CustomeClass = $ToolCustomeClassScript
-@onready var _tool_script: Node2D = $ToolScript
-@onready var _with_space: Node2D = $"With Space"
-### Automatic References Stop ###
+@onready var _unique_name: Node2D = %UniqueName
 
 
 const A_CONSTANT: int = 4
 
 func _ready() -> void:
 	pass
+@onready var _with_space: Node2D = $"With Space"
+
+@onready var _tool_custome_class_script: CustomeClass = $ToolCustomeClassScript
